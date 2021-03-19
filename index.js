7a31fd45cd1d7fd6a5860f4f00be61d97c71c43b
drslaughterrex <drslaughterrex@gmail.com>
2021-03-19 15:58:18 -0500

    completed task 5

--- index.js
+++ index.js
@@ -245,10 +245,15 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(array){
-  const =
-}
-
+function get20s(array){}
+//   let centuryArray = [];
+//     for(let i = 0; i < array.length; i++){
+//       if(array[i].years >= 1900 && array[i].years <= 2000);
+//       centuryArray.push(array[i].name);
+//     }
+//     return centuryArray;
+//   }
+// console.log(get20s())
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -260,8 +265,12 @@ function get20s(array){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(array, index){
+   const remove = array.indexOf(index);
+   if (index > -1){
+     array.splice(index, 1);
+   }
+   return array.length;
 }
    
 
@@ -281,7 +290,7 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
+function addArtist(array){
     /*Your Code Here*/
   }
 
@@ -294,7 +303,7 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
+function lotsOfArt(array){
   /*Your Code Here*/
 }
 
