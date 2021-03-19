3e827b3f7fc863c7ea997fcd3b62e0d88a33b7f3
drslaughterrex <drslaughterrex@gmail.com>
2021-03-19 14:31:44 -0500

    completed tasks 1 2 and 3

--- index.js
+++ index.js
@@ -246,7 +246,7 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
 function get20s(array){
-  /*Your Code Here*/
+  const =
 }
 
 
